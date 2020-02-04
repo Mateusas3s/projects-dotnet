6f92d399166d50fa1a96e0d3d214822dd87698df
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2020-02-04 09:40:05 -0300

    firts changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace projects_dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = 5;
+
+            for(int i=0; i < num; i++){
+                Console.WriteLine($"Bem-Vindo ao curso de .NET {i}!");
+
+            }
         }
     }
 }
