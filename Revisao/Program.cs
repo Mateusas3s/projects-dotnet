0be894bf612622925ba8de29c2d22503f46b72fc
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2020-02-07 23:07:00 -0300

    creating project review

---
+++ Revisao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opUser = Console.ReadLine();
+
+            while(opUser.ToUpper() != "X"){
+                switch(opUser){
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
