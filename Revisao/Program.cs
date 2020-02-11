ddfb57ac9293e46f04abb4bdf088e63611975969
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2020-02-11 11:58:31 -0300

    finishing project dotnet

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,6 +6,96 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var ind = 0;
+            string opUser = showMenu();
+            
+            while (opUser.ToUpper() != "X")
+            {
+                switch (opUser)
+                {
+                    case "1":
+                        Console.WriteLine("Digite o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Digite a nota do aluno: ");
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentOutOfRangeException("Valor da nota deve ser decimal!");
+                        }
+
+                        alunos[ind] = aluno;
+                        ind++;
+
+                        Console.WriteLine();
+                        break;
+
+                    case "2":
+                        foreach(var a in alunos)
+                            if(!string.IsNullOrEmpty(a.Nome))
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                        
+                        Console.WriteLine();
+                        break;
+
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nAlunos = 0;
+
+                        for (int i = 0; i < alunos.Length; i++)
+                            if(!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal += alunos[i].Nota;
+                                nAlunos++;
+                            }
+
+                        var mediaGeral = notaTotal/nAlunos;
+                        Conceito conceitoGeral = 0;
+
+                        if(mediaGeral<1)
+                        {
+                            conceitoGeral = Conceito.F; 
+                        }
+                        else if(mediaGeral<3)
+                        {
+                            conceitoGeral = Conceito.E; 
+                        }
+                        else if(mediaGeral<5)
+                        {
+                            conceitoGeral = Conceito.D; 
+                        }
+                        else if(mediaGeral<7)
+                        {
+                            conceitoGeral = Conceito.C; 
+                        }
+                        else if(mediaGeral<9)
+                        {
+                            conceitoGeral = Conceito.B; 
+                        }
+                        else if(mediaGeral<=10)
+                        {
+                            conceitoGeral = Conceito.A; 
+                        }
+
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO GERAL: {conceitoGeral}");
+
+                        Console.WriteLine();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException("Selecione uma opção válida!");
+                }
+                opUser = showMenu();
+            }
+        }
+
+        private static string showMenu()
+        {
             Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1- Inserir novo aluno");
@@ -15,19 +105,8 @@ namespace Revisao
             Console.WriteLine();
 
             string opUser = Console.ReadLine();
-
-            while(opUser.ToUpper() != "X"){
-                switch(opUser){
-                    case "1":
-                        break;
-                    case "2":
-                        break;
-                    case "3":
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
+            Console.WriteLine();
+            return opUser;
         }
     }
 }
